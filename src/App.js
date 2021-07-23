f8768e9bc6c21431738b36d21e37994f9afcd87a
ArduinoProjectPortugal <carpinfkt@hotmail.com>
2021-07-23 01:37:01 +0100

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import profile from './img/369profile.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>369 AGENCY</h1>
+      <h2>A SUA AGÊNCIA BREVEMENTE</h2>
+      <img src={profile} alt="profile" />
     </div>
   );
 }
